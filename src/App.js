2cf139fc1b7b3f849b7515341c5e1fcd54d1d74d
Petrus Hurtig <petrushurtig@hotmail.com>
2020-11-10 20:51:37 +0200

    updated index.js

--- src/App.js
+++ src/App.js
@@ -28,39 +28,6 @@ function App() {
   </BrowserRouter>
   </div>
   )
- /* const countryquery = gql`
-{
-  countries {
-    code,
-    name,
-    continent {name},
-  }
-}`;
-
-const { loading, error, data } = useQuery(countryquery);
-
-if (loading) 
-return "Loading...";
-if (error)
-return 'Error ${error.message';
-
-return (
-  <div className="App">
-      <table>
-        <tbody>
-          <th>Code</th><th>Country</th><th>Continent</th>
-          {
-          data.countries.map((country, index) => 
-          <tr key={country.code}>
-            <td>{country.code}</td>
-            <td>{country.name}</td>
-            <td>{country.continent.name}</td>
-            </tr>
-           )
-          }
-        </tbody>
-      </table>
-  </div>
-); */
+ 
 } 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
 
-const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com/',
-});
+
+
 
 ReactDOM.render(
-  <ApolloProvider client={client}><App /></ApolloProvider>,
+  <App />,
   document.getElementById('root')
 );
 
